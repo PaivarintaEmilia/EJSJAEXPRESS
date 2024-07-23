22c52cd063dcaf40fa5e0a9a9c2ddf39d24ff447
Maria Emilia Päivärinta <eemilia.paivarinta@gmail.com>
2024-07-23 17:19:28 +0200

    All functions added to the program.

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var postListing = [];
 
 // Render home page
 app.get("/", (req, res) => {
-      res.render("index.ejs");
+      res.render("index.ejs", {posts: postListing, editIndex: null});
 });
 
 // Nämä tapahtuu sitten kun saadaan postin tiedot käyttäjältä, kun tämä
@@ -35,12 +35,41 @@ app.post("/submit", (req, res) => {
 
 
 // POSTAUKSEN POISTAMINEN
-
+app.post('/delete', (req, res) => {
+    console.log(`Ekan kerran array ${postListing}`);
+    //const index = parseInt(req.body.index); // Convert index to integer
+    // Remove the row from yourDataArray (example)
+    //yourDataArray.splice(index, 1);
+    const index =  req.body["delete"]; // Saadaan poistettavan postauksen index
+    //const i =  req.body.i; // undefined
+    console.log(index);
+    
+    if (index > -1) {
+        postListing.splice(index, 1);
+    }
+    
+    console.log(postListing); // ['apple', 'banana', 'date']
+    //res.redirect('/'); // Redirect to the main page lista tyhjänä
+    res.render("index.ejs", {posts: postListing});
+});
 
 // POSTAUKSEN MUOKKAAMINEN
 
+// Kun painetaan edit painiketta, ja halutaan muokkauskenttä näkyviin
+app.post('/edit', (req, res) => {
+    const index =  req.body["edit"]; // Saadaan muokattavat postauksen index
+    res.render("index.ejs", {posts: postListing, editIndex: index});
+});
 
-// YKSITTÄISEN POSTAUKSEN NÄYTTÄMINEN ERILLISESSÄ VÄLILEHDESSÄ
-app.get("/post", (req, res) => {
-    res.render("post.ejs");
-  });
+// Kun painetaan /update painiketta, ja halutaan tallentaa muokkaus lataamalla uusi lista
+app.post('/update', (req, res) => {
+    const index =  req.body["index"]; // Saadaan muokattavat postauksen index
+    const updatedPost =  req.body["updatedPost"]; // Saadaan uusi syötetty teksti
+
+    if (index >= 0 && index < postListing.length) {
+        postListing[index] = updatedPost;
+    }
+
+    res.render("index.ejs", {posts: postListing, editIndex: null});
+});
+
